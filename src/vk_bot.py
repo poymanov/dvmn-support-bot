16985dd572b5ba7ee911771531a0202117737d0c
poymanov <n.poymanov@gmail.com>
2021-11-11 19:14:41 +0300

    Получение ответа через DialogFlow для бота группы ВК

--- src/vk_bot.py
+++ src/vk_bot.py
@@ -2,15 +2,18 @@ import os
 import random
 import vk_api as vk
 from vk_api.longpoll import VkLongPoll, VkEventType
+from dialog_flow import get_answer
 
 VK_GROUP_TOKEN = os.environ['VK_GROUP_TOKEN']
+
 
 def echo(event, vk_api):
     vk_api.messages.send(
         user_id=event.user_id,
-        message=event.text,
-        random_id=random.randint(1,1000)
+        message=get_answer(event.text, event.user_id),
+        random_id=random.randint(1, 1000)
     )
+
 
 def main():
     vk_session = vk.VkApi(token=VK_GROUP_TOKEN)
