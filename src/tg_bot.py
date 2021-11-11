ef1f71df5329d3c60e9b1d365c5c94e2c4c00f7c
poymanov <n.poymanov@gmail.com>
2021-11-11 21:47:59 +0300

    Для ботов добавлено логирование ошибок в Telegram

--- src/tg_bot.py
+++ src/tg_bot.py
@@ -1,32 +1,61 @@
 import os
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import telegram
 from dialog_flow import get_answer
 
 TELEGRAM_BOT_TOKEN = os.environ['TELEGRAM_BOT_TOKEN']
+TELEGRAM_LOGGER_BOT_TOKEN = os.environ['TELEGRAM_LOGGER_BOT_TOKEN']
+TELEGRAM_LOGGER_USER_CHAT_ID = os.environ['TELEGRAM_LOGGER_USER_CHAT_ID']
+
+logger = logging.getLogger(__file__)
 
 
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Здравствуйте")
+class TelegramLogsHandler(logging.Handler):
+    def __init__(self, tg_bot, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.tg_bot = tg_bot
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
 
 
-def answer(update, context):
-    answer = get_answer(update.message.text, update.effective_chat.id, True)
-    context.bot.send_message(chat_id=update.effective_chat.id, text=answer)
+def start_command_handler(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Здравствуйте! Чем можем помочь?')
 
 
-def main():
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+def answer_handler(update, context):
+    try:
+        answer = get_answer(update.message.text, update.effective_chat.id, True)
+        context.bot.send_message(chat_id=update.effective_chat.id, text=answer)
+    except:
+        logger.exception('Telegram-бот упал с ошибкой')
 
+
+def start_bot():
     updater = Updater(token=TELEGRAM_BOT_TOKEN)
     dispatcher = updater.dispatcher
     updater.start_polling()
 
-    start_handler = CommandHandler('start', start)
+    start_handler = CommandHandler('start', start_command_handler)
     dispatcher.add_handler(start_handler)
 
-    echo_handler = MessageHandler(Filters.text & (~Filters.command), answer)
+    echo_handler = MessageHandler(Filters.text & (~Filters.command), answer_handler)
     dispatcher.add_handler(echo_handler)
+
+
+def main():
+    logger_bot = telegram.Bot(token=TELEGRAM_LOGGER_BOT_TOKEN)
+    logger.setLevel(logging.WARNING)
+    logger.addHandler(TelegramLogsHandler(logger_bot, TELEGRAM_LOGGER_USER_CHAT_ID))
+
+    try:
+        start_bot()
+        logger.warning('Telegram-бот запущен')
+    except:
+        logger.exception('Telegram-бот упал с ошибкой')
 
 
 if __name__ == '__main__':
--- src/vk_bot.py
+++ src/vk_bot.py
@@ -1,13 +1,30 @@
 import os
+import logging
 import random
 import vk_api as vk
+import telegram
 from vk_api.longpoll import VkLongPoll, VkEventType
 from dialog_flow import get_answer
 
 VK_GROUP_TOKEN = os.environ['VK_GROUP_TOKEN']
+TELEGRAM_LOGGER_BOT_TOKEN = os.environ['TELEGRAM_LOGGER_BOT_TOKEN']
+TELEGRAM_LOGGER_USER_CHAT_ID = os.environ['TELEGRAM_LOGGER_USER_CHAT_ID']
+
+logger = logging.getLogger(__file__)
 
 
-def answer(event, vk_api):
+class TelegramLogsHandler(logging.Handler):
+    def __init__(self, tg_bot, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.tg_bot = tg_bot
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
+
+
+def answer_handler(event, vk_api):
     answer = get_answer(event.text, event.user_id, False)
 
     if answer is not None:
@@ -18,14 +35,26 @@ def answer(event, vk_api):
         )
 
 
-def main():
+def start_bot():
     vk_session = vk.VkApi(token=VK_GROUP_TOKEN)
 
     vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            answer(event, vk_api)
+            answer_handler(event, vk_api)
+
+
+def main():
+    logger_bot = telegram.Bot(token=TELEGRAM_LOGGER_BOT_TOKEN)
+    logger.setLevel(logging.WARNING)
+    logger.addHandler(TelegramLogsHandler(logger_bot, TELEGRAM_LOGGER_USER_CHAT_ID))
+
+    try:
+        logger.warning('Бот группы ВК запущен')
+        start_bot()
+    except:
+        logger.exception('Бот группы ВК упал с ошибкой')
 
 
 if __name__ == '__main__':
