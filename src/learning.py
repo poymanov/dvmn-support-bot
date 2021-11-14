0fad0996a2c8f11300dd21f1bd9029f0d0433d83
poymanov <n.poymanov@gmail.com>
2021-11-14 19:33:02 +0300

    Уточнены типы исключений для скрипта обучения бота

--- src/learning.py
+++ src/learning.py
@@ -1,6 +1,7 @@
 import os
 import json
 from dialog_flow import create_intent
+from google.api_core.exceptions import GoogleAPIError
 
 LEARNING_DATA_INTENTS_FILE = os.environ['LEARNING_DATA_INTENTS_FILE']
 
@@ -10,14 +11,14 @@ def main():
         with open(LEARNING_DATA_INTENTS_FILE, "r") as file:
             intents = json.load(file)
 
-    except:
+    except OSError:
         print('Ошибка открытия файла с вопросами')
         return
 
     for intent, data in intents.items():
         try:
             create_intent(intent, data['questions'], [data['answer']])
-        except:
+        except GoogleAPIError:
             print('Ошибка создания: {}'.format(intent))
 
 
